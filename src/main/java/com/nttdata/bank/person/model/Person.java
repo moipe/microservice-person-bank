7ea22b439c15007654bd1e3ca41a98f7e8114835
Jesus Gian Marco Castro Morales <jcasmora@emeal.nttdata.com>
2022-05-09 12:35:21 -0500

    micro-servicio-persona

---
+++ src/main/java/com/nttdata/bank/person/model/Person.java
@@ -0,0 +1,36 @@
+package com.nttdata.bank.person.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(value = "persons")
+public class Person {
+
+	@Id
+	private String _id;
+	
+	private String name;
+	
+	private String surnames;
+	
+	private int mobile;
+	
+	private String email;
+	
+	private String document;
+	
+	private String type;
+	
+	//@JsonIgnore
+	private String customerId;
+	
+}
