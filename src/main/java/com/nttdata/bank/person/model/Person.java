5a50c7025afa65ee97ad2d2ef4cf693c07c4fea0
Jesus Gian Marco Castro Morales <jcasmora@emeal.nttdata.com>
2022-05-09 22:24:36 -0500

    commit person

--- src/main/java/com/nttdata/bank/person/model/Person.java
+++ src/main/java/com/nttdata/bank/person/model/Person.java
@@ -3,6 +3,9 @@ package com.nttdata.bank.person.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 //import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
@@ -31,6 +34,7 @@ public class Person {
 	private String type;
 	
 	//@JsonIgnore
+	@JsonProperty(access = Access.WRITE_ONLY)
 	private String customerId;
 	
 }
