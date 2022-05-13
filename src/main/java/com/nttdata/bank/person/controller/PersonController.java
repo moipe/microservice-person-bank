ff3be1e28f20f7bb875c781d15c952cabfcd1d2b
Jesus Gian Marco Castro Morales <jcasmora@emeal.nttdata.com>
2022-05-13 11:05:45 -0500

    tercer commit person

--- src/main/java/com/nttdata/bank/person/controller/PersonController.java
+++ src/main/java/com/nttdata/bank/person/controller/PersonController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/person")
+//@RequestMapping("/person")
 public class PersonController {
 	
 	@Autowired
